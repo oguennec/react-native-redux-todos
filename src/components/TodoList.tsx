706177c55ff1d7063bf1d3801dc8973cacb05830
Olivier Guennec <oguennec@gmail.com>
2018-01-18 20:07:41 +0000

    arrow function to class based component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,22 +3,33 @@ import { ListView, Text, View } from 'react-native';
 import { List } from 'react-native-elements';
 import Todo from './Todo';
 
-/**
- * PropTypes definition
- */
-export interface Props {
-  onTodoClick: (id: any) => any;
+export interface ITodoListProps {
+  onTodoClick: (id: number) => any;
   todos: Array<{ completed: boolean; id: number; text: string }>;
 }
 
-const TodoList: React.StatelessComponent<Props> = ({ onTodoClick, todos }) => {
-  return (
-    <List style={{ flexDirection: 'column', flex: 6, marginTop: 20 }}>
-      {todos.map(todo => (
-        <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
-      ))}
-    </List>
-  );
-};
+export default class TodoList extends React.PureComponent<ITodoListProps> {
+  constructor(props: ITodoListProps) {
+    super(props);
+  }
 
-export default TodoList;
+  public render(): JSX.Element {
+    const { onTodoClick, todos } = this.props;
+    return (
+      <List style={{ flexDirection: 'column', flex: 6, marginTop: 20 }}>
+        {todos.map(todo => (
+          <Todo
+            key={todo.id}
+            {...todo}
+            onClick={this.onClick.bind(this, todo)}
+          />
+        ))}
+      </List>
+    );
+  }
+
+  private onClick = (todo: any) => {
+    const { onTodoClick } = this.props;
+    return onTodoClick(todo.id);
+  };
+}
